03dce2502d911e852474732ebaa3e38fbf5e1450
acdalal <adalal@carleton.edu>
2018-01-24 21:31:40 -0600

    modified psycopg2-test.py to be python 3 compliant

--- psycopg2-test.py
+++ psycopg2-test.py
@@ -1,7 +1,7 @@
 '''
     psycopg2-test.py
     Jeff Ondich, 1 Oct 2013
-    Modified by Amy Csizmar Dalal, 1 February 2016
+    Modified by Amy Csizmar Dalal, 24 January 2018
 
     This is a short example of how to access a PostgreSQL database in Python.
 '''
@@ -9,7 +9,7 @@
 import psycopg2
 import getpass
 
-# Get the database login info
+# Get the database login info. *** REPLACE THE DATABASE NAME AND USER WITH YOUR USERNAME ***
 database = 'adalal'
 user = 'adalal'
 password = getpass.getpass()
@@ -17,8 +17,8 @@ password = getpass.getpass()
 # Login to the database
 try:
     connection = psycopg2.connect(database=database, user=user, password=password)
-except Exception, e:
-    print 'Connection error: ', e
+except Exception as e:
+    print('Connection error: ', e)
     exit()
 
 # Query the database
@@ -27,14 +27,14 @@ try:
     query = 'SELECT quakedate, quaketime, mag, latitude, longitude, place  FROM earthquakes WHERE mag>7 ORDER BY mag DESC'
     cursor.execute(query)
     for row in cursor.fetchall():
-        print row
+        print(row)
 
     # An alternative to "for row in cursor.fetchall()" is "for row in cursor". The former
     # brings all the data into memory in your program, while the latter brings data in
     # small pieces (one row at a time, I think, but I haven't verified it yet).
 
-except Exception, e:
-    print 'Cursor error', e
+except Exception as e:
+    print('Cursor error', e)
     connection.close()
     exit()
 
