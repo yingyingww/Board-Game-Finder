c57b2cb9baa083587332770bd85a643910329dca
xaviert3947 <35149311+xaviert3947@users.noreply.github.com>
2018-02-06 16:08:33 -0600

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -121,7 +121,7 @@ def main():
     try:
         cursor = connection.cursor()
         gameSearch = DataSource()
-        query = gameSearch.getGamesByAgeCategoryAndTime(11,'Dice Rolling',100)
+        query = gameSearch.getGamesByAgeCategoryAndTime(11,'Card Game',100)
         cursor.execute(query)
         
         for row in cursor.fetchall():
