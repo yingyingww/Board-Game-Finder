cbc1ac8d1597d8a4ebae851f166f179205e80a89
xaviert3947 <35149311+xaviert3947@users.noreply.github.com>
2018-02-06 13:00:12 -0600

    Add files via upload

---
+++ datasource.py
@@ -0,0 +1,85 @@
+'''
+    datasource.py
+    Tresa Xavier, Calypso Leonard, Yingying Feb, 6, 2018
+    Methods to access our boardgames database.
+'''
+
+import psycopg2
+import getpass
+import random
+
+# Get the database login info. 
+database = 'xaviert'
+user = 'xaviert'
+password = getpass.getpass()
+
+# Login to the database
+try:
+    connection = psycopg2.connect(database=database, user=user, password=password)
+except Exception as e:
+    print('Connection error: ', e)
+    exit()
+
+class DataSource:
+    def getGamesByNumPlayers(self, numPlayers):
+            numPlayers = str(numPlayers)
+            query = 'SELECT game_name, avg_time, rank, category, min_age, designer FROM boardgames WHERE max_players >=' + numPlayers + 'AND min_players <= ' + numPlayers + 'ORDER BY rank ASC LIMIT 10'
+            return query
+    def getGamesByMinAge(self, inputAge):
+            inputAge = str(inputAge) 
+            query = 'SELECT game_name, avg_time, rank, category, min_age, designer FROM boardgames WHERE min_age <=' + inputAge + 'ORDER BY rank ASC'
+            return query
+    def getGamesByCategory(self, category):
+            query = 'SELECT game_name, avg_time, rank, category, min_age, designer FROM boardgames WHERE mechanic =' + str(category) + 'ORDER BY rank ASC' 
+            return query
+    def getGamesByMaxTime(self, inputTime):
+            inputTime = str(inputTime)
+            query = 'SELECT game_name, avg_time, rank, category, min_age, designer FROM boardgames WHERE max_time >= ' + inputTime + 'AND min_time <= ' + inputTime + 'ORDER BY rank ASC'
+            return query
+    
+    def getGamesByPlayersAndAge(self, numPlayers, inputAge):
+            query = 'SELECT game_name, avg_time, rank, category, min_age, designer FROM boardgames WHERE max_players >=' + str(numPlayers) + 'AND min_players <= ' + str(numPlayers) + ' AND min_age <=' + str(inputAge) + 'ORDER BY rank ASC LIMIT 10'
+        	return query
+    
+    def getRandomGame(self, RanNumber):
+            query = 'SELECT game_name, avg_time, rank, category, min_age, designer FROM boardgames WHERE rank =' + str(RanNumber)
+        
+    #def getGamesByPlayersAndCategory:
+    #def getGamesByPlayersAndTime:
+    #def getGamesByAgeAndCategory:
+    #def getGamesByAgeAndTime:
+    #def getGamesByCategoryAndTime:
+    #def getGamesByPlayersAgeAndCategory:
+    #def getGamesByPlayersAgeAndTime:
+    #def getGamesByAgeCategoryAndTime:
+    #def getGamesByAll(self, numPlayers, inputAge, category, inputTime0):
+        #numPlayers = str(numPlayers)
+        #inputAge = str(inputAge)
+        #inputTime = str(inputTime)
+        #query = 'SELECT game_name, avg_time, rank, category, min_age, designer FROM boardgames WHERE max_players >=' + numPlayers + 'AND min_players <= ' + numPlayers + 'AND min_age <=' + inputAge + ' AND mechanic =' + str(category) + 'AND  max_time >= ' + inputTime + 'AND min_time <= ' + inputTime + 'LIMIT 10 ORDER BY rank ASC'
+  
+    
+        
+# Query the database
+def main():
+    try:
+        cursor = connection.cursor()
+        gameSearch = DataSource()
+        query = gameSearch.getGamesByPlayersAndAge(2,14)
+             
+        cursor.execute(query)
+        
+        for row in cursor.fetchall():
+            print(row)
+
+
+    except Exception as e:
+        print('Cursor error', e)
+        connection.close()
+        exit()
+
+    connection.close()
+    
+    
+if __name__ == "__main__":
+    main()
