c7f3a9a06a9f9240b5ddc0d16021f3828bf824c9
xaviert3947 <35149311+xaviert3947@users.noreply.github.com>
2018-02-12 15:11:54 -0600

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -251,7 +251,7 @@ def main():
         gameSearch = DataSource()
         #query = gameSearch.getGamesByPlayersAgeAndCategory(2,12,'Card Game')
         #search(self, numPlayers, inputAge, inputCategory, inputTime)
-        query = gameSearch.search("",14,"Card Game", 60)
+        query = gameSearch.search("","","Card Game", 60)
         
         #cursor.execute(query)
         for row in query:
