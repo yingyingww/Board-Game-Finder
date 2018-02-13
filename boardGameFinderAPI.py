100795cc54e95a2419341292e09866f50e1f9a6a
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-02-13 14:46:25 -0600

    Update boardGameFinderAPI.py

--- boardGameFinderAPI.py
+++ boardGameFinderAPI.py
@@ -41,7 +41,9 @@ def result():
       inputAge = request.form['Minimum Age']
       inputNumPlayer = request.form['No. of Players']
       query = info.search(inputNumPlayer, inputAge, inputCategory, inputTime)
-      return render_template('results.html',result = query)
+      
+
+    return render_template('results.html',result = query)
           
 @app.route('/random', methods = ['POST', 'GET'])
 def random():
