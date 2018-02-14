da203df5de31ae7756255698eba7a7537a155207
Yingying Wang <35354872+yingyingww@users.noreply.github.com>
2018-02-13 18:14:14 -0600

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -273,7 +273,7 @@ def main():
     gameSearch = DataSource()
     #query = gameSearch.getGamesByPlayersAgeAndCategory(2,12,'Card Game')
     #search(self, numPlayers, inputAge, inputCategory, inputTime)
-    query = gameSearch.search(300,12,"Partnership","30")
+    #query = gameSearch.search(300,12,"Partnership","30")
         
     #cursor.execute(query)
     for row in query:
