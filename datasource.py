f05d3871d9bcb41c37192ffcc9fa8f508d6496f1
calypsoLeonard <35149366+calypsoLeonard@users.noreply.github.com>
2018-02-14 17:50:14 -0600

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -24,6 +24,14 @@ except Exception as e:
 # DataSource class to create queries based on certain criteria
 class DataSource:
     
+    
+    # Each of the following methods creates a string SQL query based on input search criteria.
+    # Parameters are the user's input criteria, of which there may be up to 4.
+    # The string query filters to ensure the games presented have the correct number of players, 
+    # are appropriate for the selected age, will not take more than the selected maximum time, 
+    # and fall into the user's selected category.
+    # If any feature was not selected, it is not used to filter the search.
+    
     #Calls for only one criterion
     
     # Just Players
@@ -33,6 +41,7 @@ class DataSource:
         min_players, max_players FROM boardgames WHERE max_players >=' + numPlayers + 'AND min_players <= ' \
         + numPlayers + 'ORDER BY rank ASC LIMIT 10'
         return query
+    
     #Just Age
     def getGamesByMinAge(self, inputAge):
         inputAge = str(inputAge)
@@ -163,15 +172,15 @@ class DataSource:
         "ORDER BY rank ASC LIMIT 10"
         return query
     
-    #if no input
+    #If no input
     def getGamesNoCriteria(self):
         query = 'SELECT game_name, avg_time, avg_rating, category, min_age, designer, image_url,\
         min_players, max_players FROM boardgames ORDER BY rank LIMIT 10'
         return query
     
-    #This method collects data for the random game button, selecting a random 
-    # game from the top 100 ranked games by 
-    #choosing a random integer and displaying information for that rank.
+    # This method creates a string query which selects a random 
+    # game from the top 100 ranked games 
+    
     def getRandomGame(self):
         ranNumber = random.randint(1,101)
         query = 'SELECT game_name, avg_time, avg_rating, category, min_age, designer, image_url,\
@@ -186,8 +195,8 @@ class DataSource:
             connection.close()
             exit()
             
-    # This method determine which getter funciton is to be used in search
-    def search(self, numPlayers, inputAge, inputCategory, inputTime):
+    # This method determines which getter funciton is to be used in search
+    def selectFuntion(self, numPlayers, inputAge, inputCategory, inputTime):
         #setting up Boolean values
         noNumPlayer = False
         noInputAge = False
@@ -235,6 +244,12 @@ class DataSource:
             action = self.getGamesByNumPlayers(numPlayers)
         if noNumPlayer and noInputAge and noInputCategory and noInputTime:
             action = self.getGamesNoCriteria()  
+        return action
+    
+    #Selects and executes SQL search
+    def search(self, numPlayers, inputAge, inputCategory, inputTime):
+        
+        action = selectFunction(numPlayers, inputAge, inputCategory, inputTime)
             
         try:
             cursor = connection.cursor()
@@ -250,17 +265,4 @@ class DataSource:
             connection.close()
             exit()
             
-def main():
-    #cursor = connection.cursor()
-    gameSearch = DataSource()
-    #query = gameSearch.getGamesByPlayersAgeAndCategory(2,12,'Card Game')
-    #search(self, numPlayers, inputAge, inputCategory, inputTime)
-    query = gameSearch.search("4","","Partnership","30")
-        
-    #cursor.execute(query)
-    for row in query:
-        print(row)
-    exit()
-            
-if __name__ == "__main__":
-    main()
+
